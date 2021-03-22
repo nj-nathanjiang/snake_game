b49a14db39e965134c23bde6d23767dcd20ea69c
njiang6 <79889759+njiang6@users.noreply.github.com>
2021-03-21 20:44:16 -0700

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -1,48 +1,57 @@
 import turtle as t
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
+print("Make sure that you don't press two keys at once, or else you will lose.")
 color = input("What color snake do you want? (red/orange/yellow/dark green/blue/purple) ").lower()
-
-size = input("How large do you want the map to be? (small/medium/large) ").lower()
-
-speed = input("How fast do you want your snake to be? (fast/medium/slow) ").lower()
-if speed == "fast":
-    speed = 25
-elif speed == "medium":
-    speed = 20
-else:
-    speed = 15
+fruit_color = input("What fruit do you want your snake to eat? Please enter the color. "
+                    "\n(apple: red/orange: orange/grape: dark magenta/blueberry: dark slate blue/watermelon: green) ")
 
 screen = t.Screen()
-if size == "medium":
-    screen.setup(width=600, height=600)
-elif size == "large":
-    screen.setup(width=800, height=800)
-else:
-    screen.setup(width=400, height=400)
+screen.setup(1200, 850)
 
 screen.bgcolor("black")
 screen.title("Snake")
 
+speed = 25
 snake = Snake(color, speed)
+food = Food(fruit_color)
+scoreboard = Scoreboard()
 screen.tracer(0)
 
+snake.make_snake()
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.up, "w")
-screen.onkey(snake.down, "s")
-screen.onkey(snake.left, "a")
-screen.onkey(snake.right, "d")
 
 game_is_on = True
 while game_is_on:
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+    screen.onkey(snake.up, "w")
+    screen.onkey(snake.down, "s")
+    screen.onkey(snake.left, "a")
+    screen.onkey(snake.right, "d")
     screen.update()
     time.sleep(0.1)
     snake.move_snake()
+
+    if snake.squares[0].distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+        scoreboard.update_scoreboard()
+
+    if snake.squares[0].xcor() > 600 or snake.squares[0].xcor() < -600 or snake.squares[0].ycor() > 425 or snake.squares[0].ycor() < -425:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for square in snake.squares[2:]:
+        if snake.head.distance(square) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
