014927f4711b30b17de4715cb33838d47700e586
njiang6 <79889759+njiang6@users.noreply.github.com>
2021-03-24 19:54:12 -0700

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -4,7 +4,14 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
+
+def game_on_false():
+    global game_is_on
+    game_is_on = False
+
+
 print("Make sure that you don't press two keys at once, or else you will lose.")
+print("Press 'e' to exit.")
 color = input("What color snake do you want? (red/orange/yellow/dark green/blue/purple) ").lower()
 fruit_color = input("What fruit do you want your snake to eat? Please enter the color. "
                     "\n(apple: red/orange: orange/grape: dark magenta/blueberry: dark slate blue/watermelon: green) ")
@@ -34,6 +41,7 @@ while game_is_on:
     screen.onkey(snake.down, "s")
     screen.onkey(snake.left, "a")
     screen.onkey(snake.right, "d")
+    screen.onkey(game_on_false, "e")
     screen.update()
     time.sleep(0.1)
     snake.move_snake()
@@ -45,13 +53,13 @@ while game_is_on:
         scoreboard.update_scoreboard()
 
     if snake.squares[0].xcor() > 600 or snake.squares[0].xcor() < -600 or snake.squares[0].ycor() > 425 or snake.squares[0].ycor() < -425:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset_game()
+        snake.reset()
 
     for square in snake.squares[2:]:
         if snake.head.distance(square) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset_game()
+            snake.reset()
 
 
 screen.exitonclick()
